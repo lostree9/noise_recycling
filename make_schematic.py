b5ebedeb0c4c8f723223da6f961152d85f6a8d97
Ayush Nadiger <122347883+lostree9@users.noreply.github.com>
2026-08-25 00:24:18 +0530

    Redesign Fig. 1 schematic

--- make_schematic.py
+++ make_schematic.py
@@ -1,95 +1,136 @@
-from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
 
-root = Path(__file__).resolve().parent
+plt.rcParams.update({
+    'text.usetex': True,
+    'font.family': 'serif',
+    'font.serif': ['Computer Modern Roman'],
+    'font.size': 8.0,
+    'axes.titlesize': 8.8,
+    'axes.labelsize': 8.0,
+    'xtick.labelsize': 6.9,
+    'lines.solid_capstyle': 'round',
+    'pdf.fonttype': 42,
+})
 
-h = 1.0
-d = 0.38
-xs = -0.95
-xi = 0.55
-fig, axs = plt.subplots(1, 3, figsize=(8.7, 3.0), gridspec_kw={"width_ratios": [1.2, 1.0, 0.9]})
+h, d = 1.0, 0.42
+xs, xi = 0.25, 2.62
+blue   = '#2F6FB0'
+green  = '#2A924A'
+red    = '#C73A36'
+purple = '#7553A6'
+orange = '#D88421'
+gray   = '#7D7D7D'
+light  = '#BEBEBE'
+black  = '#111111'
 
-# Physical billiard strip.
-ax = axs[0]
-ax.axhline(0, lw=2)
-ax.axhline(h, lw=2)
-ax.plot([xi], [d], "o", ms=7)
-ax.plot([xs], [0], "s", ms=6)
-ax.text(xi + 0.05, d + 0.03, "ion", fontsize=9)
-ax.text(xs - 0.03, -0.08, "noisy point", ha="center", va="top", fontsize=8)
-ax.text(-1.28, -0.02, r"$y=0$", va="top", fontsize=9)
-ax.text(-1.28, h + 0.02, r"$y=h$", va="bottom", fontsize=9)
-ax.annotate("", xy=(1.06, d), xytext=(1.06, 0), arrowprops=dict(arrowstyle="<->", lw=0.9))
-ax.text(1.11, d / 2, r"$d$", va="center", fontsize=9)
-ax.annotate("", xy=(1.32, h), xytext=(1.32, 0), arrowprops=dict(arrowstyle="<->", lw=0.9))
-ax.text(1.37, h / 2, r"$h$", va="center", fontsize=9)
-ax.plot([xs, xi], [0, d], lw=1.7, label="direct")
+fig = plt.figure(figsize=(7.2, 2.48))
+gs = fig.add_gridspec(1, 3, width_ratios=[1.12, 1.06, 1.14], wspace=0.18)
 
-Y1 = 2 * h - d
-xb1 = xs + (xi - xs) * (h / Y1)
-ax.plot([xs, xb1, xi], [0, h, d], lw=1.7, label="1 bounce")
+# ---------------- (a) physical strip ----------------
+ax = fig.add_subplot(gs[0,0])
+ax.set_xlim(-0.05, 3.72)
+ax.set_ylim(-0.25, 1.34)
+ax.axis('off')
+ax.text(0.00, 1.305, r'\textbf{(a) Billiard paths}', fontsize=8.8, ha='left', va='bottom')
 
-Y2 = 2 * h + d
-xb2a = xs + (xi - xs) * (h / Y2)
-xb2b = xs + (xi - xs) * (2 * h / Y2)
-ax.plot([xs, xb2a, xb2b, xi], [0, h, 0, d], lw=1.7, label="2 bounces")
-ax.scatter([xb1, xb2a], [h, h], s=15)
-ax.scatter([xb2b], [0], s=15)
-ax.text(0.02, 1.04, "billiard paths", transform=ax.transAxes, fontsize=9, weight="bold")
-ax.set_xlim(-1.34, 1.53)
-ax.set_ylim(-0.14, 1.14)
+ax.plot([0,3.24],[0,0], color=blue, lw=1.25)
+ax.plot([0,3.24],[h,h], color=blue, lw=1.25)
+ax.text(0.02, h+0.045, r'$y=h$', ha='left', va='bottom', fontsize=7.6)
+ax.text(0.02, -0.045, r'$y=0$', ha='left', va='top', fontsize=7.6)
+
+ax.scatter([xs],[0], marker='s', s=24, color=orange, zorder=5)
+ax.text(xs, -0.145, 'source', ha='center', va='top', fontsize=7.4)
+ax.scatter([xi],[d], s=28, color=blue, zorder=6)
+ax.text(xi+0.06, d+0.02, 'ion', ha='left', va='center', fontsize=7.5)
+
+ax.plot([xs,xi],[0,d], color=green, lw=1.45)
+Y1 = 2*h-d
+xb1 = xs + (xi-xs)*h/Y1
+ax.plot([xs,xb1,xi],[0,h,d], color=red, lw=1.45)
+Y2 = 2*h+d
+xb2a = xs + (xi-xs)*h/Y2
+xb2b = xs + (xi-xs)*(2*h)/Y2
+ax.plot([xs,xb2a,xb2b,xi],[0,h,0,d], color=purple, lw=1.45)
+ax.scatter([xb1],[h],s=15,color=red,zorder=6)
+ax.scatter([xb2a,xb2b],[h,0],s=15,color=purple,zorder=6)
+
+key_x0, key_x1 = 1.64, 1.98
+for yy, col, lab in zip([1.205,1.125,1.045],[green,red,purple],['direct','1 bounce','2 bounces']):
+    ax.plot([key_x0,key_x1],[yy,yy],color=col,lw=1.45,clip_on=False)
+    ax.text(key_x1+0.08,yy,lab,va='center',ha='left',fontsize=7.2)
+
+xd, xh = 3.02, 3.47
+ax.annotate('',xy=(xd,d),xytext=(xd,0),arrowprops=dict(arrowstyle='<->',lw=0.7,color=black))
+ax.text(xd+0.065,d/2,r'$d$',va='center',ha='left',fontsize=7.8)
+ax.annotate('',xy=(xh,h),xytext=(xh,0),arrowprops=dict(arrowstyle='<->',lw=0.7,color=black))
+ax.text(xh+0.065,h/2,r'$h$',va='center',ha='left',fontsize=7.8)
+
+# ---------------- (b) unfolding ----------------
+ax = fig.add_subplot(gs[0,1])
+ax.set_xlim(-0.08, 3.50)
+ax.set_ylim(-0.24, 2.72)
+ax.axis('off')
+ax.text(0.00, 2.66, r'\textbf{(b) Unfolding}', fontsize=8.8, ha='left', va='bottom')
+ax.text(0.08, 2.42, r'$\alpha_n=|d-2nh|$', fontsize=9.0, ha='left', va='center')
+
+for yy in [0,h,2*h]:
+    ax.plot([0,3.34],[yy,yy],ls=(0,(4,3)),color=blue,lw=0.75,alpha=0.72)
+
+sx=0.20
+ax.scatter([sx],[0],marker='s',s=24,color=orange,zorder=5)
+ax.text(sx,-0.14,'source',ha='center',va='top',fontsize=7.4)
+
+alphas=[d,2*h-d,2*h+d]
+cols=[green,red,purple]
+xp=[2.12,2.53,2.94]
+for a,c,x in zip(alphas,cols,xp):
+    ax.plot([sx,x],[0,a],color=c,lw=1.45)
+    ax.scatter([x],[a],s=27,color=c,zorder=5)
+
+ax.text(2.24, d+0.02, r'$\alpha_0=d$', ha='left', va='center', fontsize=7.6)
+ax.text(2.66, 2*h-d+0.01, r'$\alpha_1=2h-d$', ha='left', va='center', fontsize=7.6)
+ax.text(2.88, 2*h+d-0.13, r'$\alpha_{-1}=2h+d$', ha='right', va='top', fontsize=7.6)
+
+# ---------------- (c) depth spectrum ----------------
+ax = fig.add_subplot(gs[0,2])
+ax.set_xlim(0.0, 6.9)
+ax.set_ylim(-0.02, 1.28)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['left'].set_linewidth(0.7)
+ax.spines['bottom'].set_linewidth(0.7)
+ax.tick_params(axis='y',left=False,labelleft=False)
+ax.tick_params(axis='x',bottom=False,labelbottom=False)
+ax.set_title(r'\textbf{(c) Return-depth spectrum}', loc='left', fontsize=8.8, pad=3)
+
+nseq=np.array([0,1,-1,2,-2,3,-3])
+a=np.abs(d-2*nseq*h)
+k=0.34
+w=np.exp(-k*a)
+ord=np.argsort(a)
+a,w=a[ord],w[ord]
+
+for i,(aa,ww) in enumerate(zip(a,w)):
+    col=[green,red,purple][i] if i<3 else gray
+    stem_col=blue if i<3 else light
+    ax.vlines(aa,0,ww,color=stem_col,lw=1.05,zorder=1)
+    ax.scatter([aa],[ww],s=20,color=col,zorder=3)
+xx=np.linspace(0.25,6.65,240)
+ax.plot(xx,np.exp(-k*xx),color=light,lw=0.75,ls='--',zorder=0)
+
 ax.set_xticks([])
-ax.set_yticks([])
-for spine in ax.spines.values():
-    spine.set_visible(False)
-ax.legend(frameon=False, fontsize=7.2, loc="upper center", bbox_to_anchor=(0.60, 0.98))
+ax.set_xlabel(r'unfolded depth $a$',labelpad=3)
+ax.text(d+0.10, w[0]+0.01, r'$a=d$', ha='left', va='center', fontsize=7.0)
+ax.text((2*h-d)+0.12, w[1]+0.01, r'$a=2h-d$', ha='left', va='center', fontsize=7.0)
+ax.text((2*h+d)+0.14, w[2]-0.02, r'$a=2h+d$', ha='left', va='center', fontsize=7.0)
 
-# Unfolded image copies.
-ax = axs[1]
-for y in [0, h, 2 * h, 3 * h]:
-    ax.axhline(y, lw=0.75, ls="--", alpha=0.7)
-ax.plot([0], [0], "s", ms=6)
-ax.text(0, -0.17, "source", ha="center", fontsize=8)
-alphas = [d, 2 * h - d, 2 * h + d]
-labels = [r"$d$", r"$2h-d$", r"$2h+d$"]
-xt = [0.72, 0.94, 1.16]
-for a, label, x in zip(alphas, labels, xt):
-    ax.plot([x], [a], "o", ms=6)
-    ax.plot([0, x], [0, a], lw=1.5)
-    ax.annotate("", xy=(x + 0.12, a), xytext=(x + 0.12, 0), arrowprops=dict(arrowstyle="<->", lw=0.75))
-    ax.text(x + 0.16, a / 2, label, rotation=90, va="center", fontsize=7.5)
-ax.text(0.02, 1.04, "unfolding", transform=ax.transAxes, fontsize=9, weight="bold")
-ax.text(0.03, 0.93, r"$\alpha_n=|d-2nh|$", transform=ax.transAxes, fontsize=9)
-ax.set_xlim(-0.15, 1.55)
-ax.set_ylim(-0.14, 2.65)
-ax.set_xticks([])
-ax.set_yticks([])
-for spine in ax.spines.values():
-    spine.set_visible(False)
+ax.text(0.06,0.92,r'$\mu_h=\sum_n\delta_{\alpha_n}$',transform=ax.transAxes,
+        ha='left',va='top',fontsize=8.3)
+ax.text(0.52,0.82,r'Laplace weight $e^{-ka}$',transform=ax.transAxes,
+        ha='left',va='top',fontsize=7.4)
 
-# Return-depth spectrum.
-ax = axs[2]
-N = 3
-ns = np.arange(-N, N + 1)
-a = np.abs(d - 2 * ns * h)
-order = np.argsort(a)
-a = a[order]
-for aa in a:
-    height = np.exp(-0.42 * aa)
-    ax.vlines(aa, 0, height, lw=1.7)
-    ax.plot([aa], [height], "o", ms=4)
-for aa, label in zip([d, 2 * h - d, 2 * h + d], [r"$d$", r"$2h-d$", r"$2h+d$"]):
-    ax.text(aa, 0.04, label, rotation=90, ha="center", va="bottom", fontsize=7.3)
-ax.set_xlabel(r"unfolded depth $a$", fontsize=8.5)
-ax.set_ylabel(r"weight $e^{-ka}$", fontsize=8.5)
-ax.set_yticks([])
-ax.tick_params(axis="x", labelsize=7)
-ax.text(0.02, 1.04, "return-depth spectrum", transform=ax.transAxes, fontsize=9, weight="bold")
-ax.text(0.05, 0.91, r"$\mu_h=\sum_n\delta_{\alpha_n}$", transform=ax.transAxes, fontsize=8.5)
-ax.spines["top"].set_visible(False)
-ax.spines["right"].set_visible(False)
-
-fig.tight_layout(pad=0.55, w_pad=0.7)
-fig.savefig(root / "fig_unfolding_schematic.pdf", bbox_inches="tight")
-fig.savefig(root / "fig_unfolding_schematic.png", dpi=240, bbox_inches="tight")
+fig.subplots_adjust(left=0.012,right=0.995,bottom=0.16,top=0.965)
+root='.'
+fig.savefig(root+'/fig_unfolding_schematic.pdf',bbox_inches='tight',pad_inches=0.01)
+fig.savefig(root+'/fig_unfolding_schematic.png',dpi=360,bbox_inches='tight',pad_inches=0.01)
